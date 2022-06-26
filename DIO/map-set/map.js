934ca48e3fe01f722b3d0acae4736cd924e3230a
Leonardo <leonardophotoshop1403@gmail.com>
2022-06-26 10:35:33 -0300

    v8 map and set

---
+++ DIO/map-set/map.js
@@ -0,0 +1,16 @@
+const myMap = new Map();
+
+
+myMap.set('apple', 'fruit');
+//Map(1) {"apple" => "fruit"}
+
+myMap.get('apple');
+// "fruit "
+
+myMap.delete('apple');
+// true 
+
+myMap.get('apple')
+// undefined
+
+console.log(myMap)
